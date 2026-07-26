6e56bfc790f8721bd6e9823fb26c860b2adb941a
AmlElemam <amlelemam27@gmail.com>
2026-07-27 00:33:28 +0300

    day 4

--- src/main/java/day4/Main.java
+++ src/main/java/day4/Main.java
@@ -1,26 +1,48 @@
 package day4;
 
 public class Main {
-    void main(String[] args) {
+    public static void main(String[] args) {
 
-       Sum s = new Sum();
-       s.sum2Number(10,20);
-       s.sum2Number(10,40);
+//       Sum s = new Sum();
+//       s.sum2Number(10,20);
+//       s.sum2Number(10,40);
+//
+//       hamda();
+//       total(10,20);
 
-       hamda();
-       total(10,20);
+        Rectangle rectangle = new Rectangle();
+        double x = rectangle.calculateArea();
+
+
+        Rectangle r1 = new Rectangle();
+        r1.setLength(5);
+        r1.setWidth(5);
+        double areaOFRoom1 = r1.calculateArea();
+
+        System.out.println("The area of Room 1 is: " + areaOFRoom1);
+
+        Rectangle r2 = new Rectangle();
+        r2.setLength(10.5);
+        r2.setWidth(10.5);
+        double areaOfRoom2 = r2.calculateArea();
+        System.out.println("The area of Room 2 is: " + areaOfRoom2);
+
+        System.out.println("___________________________");
+        double totalArea = areaOFRoom1 + areaOfRoom2;
+        System.out.println("The total space of room 1 and room 2 is: " + totalArea);
+
     }
-
-    public static void hamda()
-    {
-        System.out.println("Hello, Hamda");
-    }
-
-    public  void total(int x,int y) {
-
-        int sum = x + y;
-
-        System.out.println("The total is " + sum);
-    }
+//
+//    public static void hamda()
+//    {
+//        System.out.println("Hello, Hamda");
+//    }
+//
+//    public  void total(int x,int y) {
+//
+//        int sum = x + y;
+//
+//        System.out.println("The total is " + sum);
+//    }
 
 }
---
+++ src/main/java/day4/Rectangle.java
@@ -0,0 +1,47 @@
+package day4;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    // Scanner input = new Scanner(System.in);
+
+    private double length; // field
+    private double width; // field
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width >0)
+            this.width = width;
+        else
+            System.out.println("Please enter a valid width");
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if (length >0)
+            this.length = length;
+        else
+            System.out.println("Please enter a valid length");
+
+    }
+
+    public double calculateArea(){
+        return width*length;
+    }
+
+//    public double calculateArea() {
+//        System.out.println("Enter length of rectangle");
+//        length = input.nextDouble();
+//        System.out.println("Enter width of rectangle");
+//        width = input.nextDouble();
+//        double area = length * width;
+//        System.out.println("The area of rectangle is " + area);
+//        return area;
+//    }
+}
